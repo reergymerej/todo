64dcb1a4cd2845ed740d30795b3eaed41122d668
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-06-28 23:05:24 -0400

    remove junk specs

--- spec/todo-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-// import {expect} from 'chai';
-//
-// xdescribe('life', () => {
-//   it('is not fair', () => {
-//     expect(false).to.equal(true);
-//   });
-// });
--- spec/todo-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-TodoView = require '../lib/todo-view'
-
-xdescribe "TodoView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
